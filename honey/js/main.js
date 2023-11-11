7669117819af08271536789b54ba7ff8912ab867
BlokhSerhii <0997198431@ukr.net>
2023-11-11 23:12:28 +0200

    Add Honey

---
+++ honey/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.cards__row').slick({
+      setting: setting-value
+    });
+  });
+  
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
